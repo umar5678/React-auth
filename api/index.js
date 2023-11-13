eb19ddf3377c0d2bd852f64b7f45956b9ff26d58
Umar Farooq <umarfarooq567800@gmail.com>
2023-11-13 17:28:35 +0500

    database established and connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3344, () => {
   console.log(`server is listening on port 3344`);
