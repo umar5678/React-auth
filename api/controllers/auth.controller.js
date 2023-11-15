24e3510ddd31488c84dbc427c31d3fe7350c77ec
Umar Farooq <umarfarooq567800@gmail.com>
2023-11-15 16:14:38 +0500

    login route completed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -15,6 +17,25 @@ export const signup = async (req, res, next) => {
     res.status(201).json({ msg: "user created successfully" });
   } catch (err) {
     console.log(err);
+    next(err);
+  }
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPswd = bcryptjs.compareSync(password, validUser.password);
+    if (!validPswd) return next(errorHandler(401, "Wrong credintials"));
+    const { password: hashedPswd, ...rest } = validUser._doc;
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const expireTime = new Date(Date.now() + 3600000);
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expireTime })
+      .status(200)
+      .json(rest);
+  } catch (err) {
     next(err);
   }
 };
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,4 +1,3 @@
-import { set } from "mongoose";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
