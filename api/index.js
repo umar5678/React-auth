edfa0a1f4e414afe9c2cd7804dd631dcdd350fb8
Umar Farooq <umarfarooq567800@gmail.com>
2023-11-24 17:26:28 +0500

    created updaed user API route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.listen(3344, () => {
   console.log(`server is listening on port 3344`);
