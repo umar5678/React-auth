c5cdfcff037d316bf889c92418c321b81aacc64b
Umar Farooq <umarfarooq567800@gmail.com>
2023-11-25 17:01:48 +0500

    finish development added home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className=" mx-auto mt-4 px-4 max-w-[600px]">
+      <h1 className="text-3xl font-bold my-2">About Page</h1>
+      <p className="my-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+        consequatur ullam tempore ipsam ratione perspiciatis distinctio quae
+        repellendus blanditiis dicta, nisi enim fuga, quisquam natus labore
+        voluptas sed ea molestias neque debitis dolorum eos optio voluptatibus
+        facere! Voluptatibus, explicabo eum.
+      </p>
+      <p className="my-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+        consequatur ullam tempore ipsam ratione perspiciatis distinctio quae
+        repellendus blanditiis dicta, nisi enim fuga, quisquam natus labore
+        voluptas dolorum eos optio voluptatibus facere! Voluptatibus, explicabo
+        eum.
+      </p>
+      <p className="my-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+        consequatur ullam tempore ipsam ratione perspiciatis distinctio quae
+        repellendus blanditiis dicta, nisi enim fuga, quisquam natus labore
+        voluptas sed ea molestias neque debitis dolorum eos optio voluptatibus
+        facere! Voluptatibus, explicabo eum. Lorem ipsum dolor sit.
+      </p>
+    </div>
+  );
 };
 
 export default About;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,30 @@
-import React from "react";
-
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className=" mx-auto mt-4 px-4 max-w-[600px]">
+      <h1 className="text-3xl font-bold my-2">MERN Auth Project</h1>
+      <p className="my-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+        consequatur ullam tempore ipsam ratione perspiciatis distinctio quae
+        repellendus blanditiis dicta, nisi enim fuga, quisquam natus labore
+        voluptas sed ea molestias neque debitis dolorum eos optio voluptatibus
+        facere! Voluptatibus, explicabo eum.
+      </p>
+      <p className="my-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+        consequatur ullam tempore ipsam ratione perspiciatis distinctio quae
+        repellendus blanditiis dicta, nisi enim fuga, quisquam natus labore
+        voluptas dolorum eos optio voluptatibus facere! Voluptatibus, explicabo
+        eum.
+      </p>
+      <p className="my-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+        consequatur ullam tempore ipsam ratione perspiciatis distinctio quae
+        repellendus blanditiis dicta, nisi enim fuga, quisquam natus labore
+        voluptas sed ea molestias neque debitis dolorum eos optio voluptatibus
+        facere! Voluptatibus, explicabo eum. Lorem ipsum dolor sit.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
